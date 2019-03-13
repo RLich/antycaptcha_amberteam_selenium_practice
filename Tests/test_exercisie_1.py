b4382825b367fe6111f12770069eb22bda38ddde
rlichocki <rafal.lichocki@gmail.com>
2019-03-13 17:01:06 +0100

    exercise 1

--- Tests/test_exercisie_1.py
+++ Tests/test_exercisie_1.py
@@ -1,4 +1,11 @@
+import time
+
 def test_exercise(app):
     app.open_home_page()
-    app.enter_exercise(1)
+    app.enter_exercise("1")
     assert app.get_page_title() == "Exercise 1 - Two buttons"
+
+    app.push_buttons_in_proper_order()
+    time.sleep(3)
+    assert app.get_solution_ex_1() == "OK. Good answer"
+    time.sleep(3)
--- fixture/application.py
+++ fixture/application.py
@@ -13,19 +13,37 @@ class Application:
 
     def enter_exercise(self, number):
         wd = self.wd
-        string = "EXERCISE %s" %number
+        string = "EXERCISE %s" % str(number)
         exercises = wd.find_elements_by_xpath("//a[@class='button u-full-width ']")
         for exercise in exercises:
             if string in exercise.text:
                 exercise.click()
+                break
 
     def get_page_title(self):
         wd = self.wd
         page_title = wd.find_element_by_xpath("//h1[@class='title']")
-        return page_title
+        return page_title.text
 
-    def check_what_buttons_to_push(self):
-        pass
+    def push_buttons_in_proper_order(self):
+        wd = self.wd
+        get_buttons = wd.find_elements_by_xpath("//tbody//code")
+        button_1 = get_buttons[0]
+        button_2 = get_buttons[1]
+        button_3 = get_buttons[2]
+        print(button_1.text, button_2.text, button_3.text)
+
+        button_1.click()
+        button_2.click()
+        button_3.click()
+
+    def get_solution_ex_1(self):
+        wd = self.wd
+        wd.find_element_by_id("solution").click()
+        solution = wd.find_element_by_xpath("//div//code[@class='wrap']")
+        return solution.text
+
+
 
     def destroy(self):
         self.wd.quit()
